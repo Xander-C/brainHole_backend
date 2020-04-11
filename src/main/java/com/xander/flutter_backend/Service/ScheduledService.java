8da81586f21b58a85f5f5afb2c48b1039da485cc
Xander-C <1019104544@qq.com>
2020-04-11 18:44:30 +0800

    feat: weather push

--- src/main/java/com/xander/flutter_backend/Service/ScheduledService.java
+++ src/main/java/com/xander/flutter_backend/Service/ScheduledService.java
@@ -11,11 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 @Configuration
